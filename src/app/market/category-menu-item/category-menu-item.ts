55576a60417b124c587d695a601e07ee02906d7e
Janella Web <quizonwebsites@gmail.com>
2026-05-10 17:27:18 -0600

    Added the Components

---
+++ src/app/market/category-menu-item/category-menu-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-menu-item',
+  imports: [],
+  templateUrl: './category-menu-item.html',
+  styleUrl: './category-menu-item.css',
+})
+export class CategoryMenuItem {}
---
+++ src/app/market/category-menu/category-menu.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-menu',
+  imports: [],
+  templateUrl: './category-menu.html',
+  styleUrl: './category-menu.css',
+})
+export class CategoryMenu {}
